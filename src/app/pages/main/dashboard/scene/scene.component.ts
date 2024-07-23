ac5329c5344ae226295682e48a1dc808d2e0fd1a
Syukur Bin Md Kassim <syukur.kassim@tmrnd.com.my>
2024-07-23 21:42:30 +0800

    Add scene

--- src/app/pages/main/dashboard/scene/scene.component.ts
+++ src/app/pages/main/dashboard/scene/scene.component.ts
@@ -1,17 +1,49 @@
 import { Component } from '@angular/core';
 import { SceneService } from '../../../../services/scene.service';
-import { Observable } from 'rxjs';
+import { map, Observable, shareReplay, Subject, takeUntil } from 'rxjs';
 import { Scene } from '../../../../types/scene.type';
+import { AsyncPipe, NgClass, TitleCasePipe } from '@angular/common';
+import { MatCard, MatCardContent } from '@angular/material/card';
+import { MatRipple } from '@angular/material/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-scene',
   standalone: true,
-  imports: [],
+  imports: [
+    MatCard,
+    MatCardContent,
+    MatRipple,
+    AsyncPipe,
+    TitleCasePipe,
+    NgClass,
+  ],
   templateUrl: './scene.component.html',
-  styleUrl: './scene.component.scss'
+  styleUrl: './scene.component.scss',
 })
 export class SceneComponent {
-  scene$!: Observable<Scene>;
+  scenes$!: Observable<Scene[]>;
+  isHandset$!: Observable<boolean>;
 
-  constructor(private sceneService: SceneService) {}
+  destroyed = new Subject<void>();
+
+  constructor(
+    private sceneService: SceneService,
+    private breakpointObserver: BreakpointObserver,
+  ) {
+    this.initObservable();
+  }
+
+  initObservable() {
+    this.scenes$ = this.sceneService.getAll();
+    this.isHandset$ = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
+      map((result) => result.matches),
+      shareReplay(),
+      takeUntil(this.destroyed),
+    );
+  }
+
+  onClicked(scene: Scene) {
+    
+  }
 }
