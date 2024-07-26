b6c29a06fb13e71a7d0bcfa61eb5aaa5e76e1aeb
Syukur Bin Md Kassim <syukur.kassim@tmrnd.com.my>
2024-07-26 12:13:43 +0800

    Added device main page implementation

--- src/app/pages/main/admin/device/device.component.ts
+++ src/app/pages/main/admin/device/device.component.ts
@@ -1,12 +1,46 @@
-import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { Component, Sanitizer } from '@angular/core';
+import { MatFabButton } from '@angular/material/button';
+import { MatCard } from '@angular/material/card';
+import { MatIcon, MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
+import { RouterOutlet } from '@angular/router';
+import { registerIcon } from '../../../../functions/register-icon.func';
 
 @Component({
   selector: 'app-device',
   standalone: true,
-  imports: [],
+  imports: [MatCard, MatFabButton, MatIcon, RouterOutlet, NgClass],
   templateUrl: './device.component.html',
-  styleUrl: './device.component.scss'
+  styleUrl: './device.component.scss',
 })
 export class DeviceComponent {
+  active = 'fan';
 
+  devices = [
+    {
+      name: 'Fans',
+      icon: 'fan',
+    },
+    {
+      name: 'Light',
+      icon: 'light',
+    },
+    {
+      name: 'Switch',
+      icon: 'switch',
+    },
+  ];
+
+  constructor(sanitizer: DomSanitizer, iconRegistery: MatIconRegistry) {
+    registerIcon(
+      this.devices.map((x) => x.icon),
+      sanitizer,
+      iconRegistery,
+    );
+  }
+
+  onClick(icon: string) {
+    this.active = icon;
+  }
 }
